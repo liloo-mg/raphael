3643d3aaad67927ce5505846f207e25f7f3635a6
Liloo <cp02@kawa-group.fr>
2021-12-09 14:35:57 +0300

    retour: couleur active, changement de photo

--- includes/config.php
+++ includes/config.php
@@ -11,7 +11,7 @@ function config($key = '')
 {
     $config = [
         'name' => 'Sexe Coquine',
-        'site_url' => 'http://localhost/sexe_coquine',
+        'site_url' => 'http://localhost/raphael',
         'pretty_uri' => true,
         'nav_menu' => [
             '' => 'Accueil',
@@ -23,6 +23,7 @@ function config($key = '')
         'template_path' => 'template',
         'content_path' => 'content',
         'version' => 'v1.0',
+        'number_random' => rand(15, 150)
     ];
 
     return isset($config[$key]) ? $config[$key] : null;
